025bbde17912dde7896ef594e221aff8505187e5
Lizi_S <liza.stepanova99@gmail.com>
2025-02-24 11:45:04 +0100

    update code to ES modules and arrow functions

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,9 @@
-export const countContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+
+export const countContacts = async () => {
+  const contacts = await readContacts();
+    return `Total contacts: ${contacts.length}`;
+};
 
 console.log(await countContacts());
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,14 @@
-const generateContacts = async (number) => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
+
+export const generateContacts = async (number = 5) => {
+  const contacts = await readContacts();
+    for (let i = 0; i < number; i++) {
+        contacts.push(createFakeContact());
+    }
+    await writeContacts(contacts);
+    console.log(`Added ${number} new contacts.`);
+};
 
 generateContacts(5);
